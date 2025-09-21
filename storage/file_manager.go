e3cf184bb881b7a9ac589d22cd158e092f717041
“mirzoxid” <mirzoxidadxamjanov01@gmail.com>
2025-09-21 17:59:19 +0500

    filemanager and table manager have added

---
+++ storage/file_manager.go
@@ -0,0 +1,89 @@
+package storage
+
+import (
+	"io"
+	"os"
+	"sync"
+)
+
+type FileManager struct {
+	file *os.File
+	lock sync.Mutex
+}
+
+func NewFileManager(filePath string) (*FileManager, error) {
+	file, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, err
+	}
+	return &FileManager{file: file}, nil
+}
+
+func (fm *FileManager) Write(offset int64, data []byte) error {
+	fm.lock.Lock()
+	defer fm.lock.Unlock()
+	_, err := fm.file.WriteAt(data, offset)
+	if err != nil {
+		return err
+	}
+	return fm.file.Sync()
+}
+
+func (fm *FileManager) Read(offset int64, size int) ([]byte, error) {
+	fm.lock.Lock()
+	defer fm.lock.Unlock()
+	data := make([]byte, size)
+	n, err := fm.file.ReadAt(data, offset)
+	if err != nil {
+		return nil, err
+	}
+	return data[:n], nil
+}
+
+func (fm *FileManager) ReadAll() ([]byte, error) {
+	fm.lock.Lock()
+	defer fm.lock.Unlock()
+
+	if _, err := fm.file.Seek(0, io.SeekStart); err != nil {
+		return nil, err
+	}
+
+	return io.ReadAll(fm.file)
+}
+
+func (fm *FileManager) Close() error {
+	fm.lock.Lock()
+	defer fm.lock.Unlock()
+	return fm.file.Close()
+}
+
+func (fm *FileManager) FileExists(name string) (bool, error) {
+	fm.lock.Lock()
+	defer fm.lock.Unlock()
+	_, err := os.Stat(name)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func (fm *FileManager) CreateFile(name string) (*os.File, error) {
+	fm.lock.Lock()
+	defer fm.lock.Unlock()
+	return os.Create(name)
+}
+
+func (fm *FileManager) DeleteFile(name string) error {
+	fm.lock.Lock()
+	defer fm.lock.Unlock()
+	return os.Remove(name)
+}
+
+func (fm *FileManager) OpenFile(name string) (*os.File, error) {
+	fm.lock.Lock()
+	defer fm.lock.Unlock()
+	return os.OpenFile(name, os.O_RDWR, 0666)
+}
--- storage/table.go
+++ storage/table.go
@@ -1,5 +1,9 @@
 package storage
 
+import (
+	"errors"
+)
+
 type ColumnType int
 
 const (
@@ -23,4 +27,63 @@ type Record struct {
 
 type Item struct {
 	Literal interface{}
+}
+
+type TableManager struct {
+	fileManager *FileManager
+}
+
+func NewTableManager(filePath string) (*TableManager, error) {
+	file_manager, err := NewFileManager(filePath)
+	if err != nil {
+		panic(err.Error())
+	}
+	return &TableManager{fileManager: file_manager}, nil
+}
+
+func (tm *TableManager) CreateTable(name string, schema *Schema) error {
+	exist, err := tm.fileManager.FileExists(name + ".schema")
+
+	if err != nil {
+		return err
+	}
+
+	if exist {
+		return errors.New("file already exist")
+	}
+
+	file, err := tm.fileManager.CreateFile(name + ".schema")
+
+	if err != nil {
+		return err
+	}
+
+	serialized_schema := SerializeSchema(schema)
+	_, err = file.Write(serialized_schema)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (tm *TableManager) GetTable(name string) (schema Schema, err error) {
+	schema = Schema{}
+	exist, err := tm.fileManager.FileExists(name)
+	if err != nil {
+		return schema, err
+	}
+
+	if !exist {
+		return schema, errors.New("table does not exist")
+	}
+
+	data, err := tm.fileManager.ReadAll()
+	if err != nil {
+		return schema, err
+	}
+	schema = DeserializeSchema(data)
+
+	return schema, nil
 }
